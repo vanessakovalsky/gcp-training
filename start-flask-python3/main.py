cf01931663d4d58b032df5cdd5db116ded7699fb
vanessakovalsky <v.david@kovalibre.com>
2023-03-29 07:58:19 +0200

    Add directory for working with flask update to python3

---
+++ start-flask-python3/main.py
@@ -0,0 +1,45 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START mod0_baseline]
+from flask import Flask, render_template, request
+from google.cloud import ndb
+
+app = Flask(__name__)
+ds_client = ndb.Client()
+
+class Visit(ndb.Model):
+    'Visit entity registers visitor IP address & timestamp'
+    visitor   = ndb.StringProperty()
+    timestamp = ndb.DateTimeProperty(auto_now_add=True)
+
+def store_visit(remote_addr, user_agent):
+    'create new Visit entity in Datastore'
+    Visit(visitor='{}: {}'.format(remote_addr, user_agent)).put()
+
+def fetch_visits(limit):
+    'get most recent visits'
+    return Visit.query().order(-Visit.timestamp).fetch(limit)
+
+@app.route('/')
+def root():
+    with ds_client.context():
+        store_visit(request.remote_addr, request.user_agent)
+        visits = fetch_visits(10)
+        return render_template('index.html', visits=visits)
+    
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
